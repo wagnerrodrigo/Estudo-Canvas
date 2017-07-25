11ce444e69f19bc8626bc51b7aa9d30c946fcb3f
“wagnerrodrigo.pan@gmail.comgit config --global user.eamil “wagnerrodrigo.pan@gmail.com <wagnerrodrigo.pan@gmail.com>
2017-07-24 21:41:01 -0300

    add mais itens

--- Cap2/Minhaanimacao.js
+++ Cap2/Minhaanimacao.js
@@ -1,11 +1,50 @@
+//Construtor
 function Animacao() {
-	// body...
+	this.context = context;
+	this.sprites =[];
+	this.ligado = false; // Atributo
 }
+// prototype
 Animacao.prototype ={
-	novoSprite: function(sprites){
-
+	novoSprite: function(sprite){
+		this.sprites.push(sprite);
 	},
+// Método
 	ligar: function(){
-		
+		this.ligado = true;
+		this.proximoFrame();
+	},
+// Método
+	desligar: function(){
+		this.ligado = false;
 	}
+
+},
+	proximoFrame: function(){
+	// Posso Continuar? ooo Mae eu posso ir brincar hahahahaha
+	if ( ! this.ligado ) return;
+
+	// a cada ciclo, ligamos a tela ou desenhamos um fundo
+	this.limparTela();
+
+	// Atualizamos o estado dos sprites
+	for (var i in this.sprites)
+		this.sprites[i].atualizar();
+
+	// desenhamos os sprites
+	for (var i in this.sprites)
+		this.sprites[i].desenhar();
+
+	// Chamamos o próximo ciclo
+	var animacao = this;
+	requestAnimationFrame(function() {
+		animacao.proximoFrame();
+	});
+
+	// limpar a tela
+	limparTela: function() {
+		var ctx = this.context; // Só para facilitar a escrita
+		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+	}
+
 }
