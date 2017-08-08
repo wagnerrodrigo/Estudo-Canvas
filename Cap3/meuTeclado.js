8c240ac6f9af97b0072a49a54e21e26a92cc1e0d
Wagner Rodrigo Da Silva <wagnerrodrigo.pan@gmail.com>
2017-08-08 17:09:36 -0300

    achei o erroclearclear

--- Cap3/meuTeclado.js
+++ Cap3/meuTeclado.js
@@ -12,13 +12,21 @@ function Teclado(elemento){
 	var teclado = this;
 	elemento.addEventListener('keydown', function(evento){
 		teclado.pressionadas[evento.keyCode] = true;
+		// bom aqui esta funcionado console.log("tecla pressionada");
 	});
-	elemento.addEventListener('keyup',function(evento){
+	
+	elemento.addEventListener('keyup', 	function(evento){
 		teclado.pressionadas[evento.keyCode] = false;
+		// bom aqui esta funcionado tambem console.log("telaca solta");
 	});
+	
+	// estava dando erro porque o prototype estava junto do evento !!!!!
+	}
+	// o prototype tem que esta junto da funçao e nao no evento!!!!!!
 	Teclado.prototype = {
 		pressionada: function(tecla) {
+			//  teste se estava funcionado console.log(SETA_ESQUERDA);
+			// teste se estava funcionado console.log(SETA_DIREITA);
 			return this.pressionadas[tecla];
 		}
-	}
 }
